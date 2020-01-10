82a63586b3200046a235f57428b14c09e5ff1ad8
xmcp <xiaoyuanan2000@sina.com>
2020-01-10 13:37:06 +0800

    update semester

--- src/config.js
+++ src/config.js
@@ -1,17 +1,16 @@
 export const SEMESTER={
-    name: '2019-2020 第一学期',
-    id: '2019-2020 sem1', // checked before importing
-    begin_date: new Date(2019,9-1,9, 0,0,0,0),
+    name: '2019-2020 第二学期',
+    id: '2019-2020 sem2', // checked before importing
+    begin_date: new Date(2020,2-1,17, 0,0,0,0),
     weeks: 16,
     exclude_dates: [
-        new Date(2019,9-1,13, 0,0,0,0), // 中秋
-        new Date(2019,9-1,30, 0,0,0,0), // 国庆
-        new Date(2019,10-1,1, 0,0,0,0),
-        new Date(2019,10-1,2, 0,0,0,0),
-        new Date(2019,10-1,3, 0,0,0,0),
-        new Date(2019,10-1,4, 0,0,0,0),
-        new Date(2019,10-1,5, 0,0,0,0),
-        new Date(2019,10-1,6, 0,0,0,0),
+        new Date(2020,4-1,24, 0,0,0,0), // 运动会
+        new Date(2020,5-1,1, 0,0,0,0), // 五一
+        new Date(2020,5-1,4, 0,0,0,0),
+        new Date(2020,5-1,5, 0,0,0,0),
+        new Date(2020,5-1,6, 0,0,0,0),
+        new Date(2020,5-1,7, 0,0,0,0),
+        new Date(2020,5-1,8, 0,0,0,0),
     ]
 };
 
