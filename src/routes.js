7cd365a8de2ce751b76e4e6d0340f5fa33e4793d
xmcp <xmcp@pku.edu.cn>
2022-06-25 20:49:45 +0800

    remove isop import code

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,6 @@ import {PageHeader} from 'antd';
 
 import {Homepage} from './homepage/Homepage';
 import {ImportElective} from './import/ImportElective';
-import {ImportIsop} from './import/ImportIsop';
 import {ImportConfig} from './import/ImportConfig';
 import {Edit} from './edit/Edit';
 import {ExportIcs} from './export/ExportIcs';
@@ -12,7 +11,6 @@ import {About} from './about/About';
 export const ROUTES={
     homepage: 'homepage',
     import_elective: 'import_elective',
-    import_isop: 'import_isop',
     import_config: 'import_config',
     edit: 'edit',
     export_ics: 'export_ics',
@@ -22,7 +20,6 @@ export const ROUTES={
 export const PAGE={
     homepage: Homepage,
     import_elective: ImportElective,
-    import_isop: ImportIsop,
     import_config: ImportConfig,
     edit: Edit,
     export_ics: ExportIcs,
