c032efc7ebe30cfcd3e2d36143601169e896ba6d
xmcp <xmcp@pku.edu.cn>
2022-07-07 18:04:53 +0800

    hide `about` link in footer in `about` route

--- src/shared/Footer.js
+++ src/shared/Footer.js
@@ -7,7 +7,11 @@ const {Footer}=Layout;
 export function FooterComp(props) {
     return (
         <Footer style={{textAlign: 'center', backgroundColor: 'transparent'}}>
-            <a onClick={()=>{props.navigate(ROUTES.about);}}>关于课表助手</a> by @xmcp
+            {props.current_route===ROUTES.about ?
+                '课表助手' :
+                <a onClick={()=>{props.navigate(ROUTES.about);}}>关于课表助手</a>
+            }{' '}
+            by @xmcp
         </Footer>
     );
 }
